a9339ddca65507ce167d8677c35e125d73b6ab7b
Impe11e <1mpellee@gmail.com>
2025-10-28 11:41:51 +0200

    fix: minor shape fixed

--- Lab4/Lab4/shape.cpp
+++ Lab4/Lab4/shape.cpp
@@ -73,34 +73,37 @@ void CubeShape::Show(HDC hdc)
 	long right = max(xs1, xs2);
 	long top = min(ys1, ys2);
 	long bottom = max(ys1, ys2);
-	
+
 	long width = right - left;
 	long height = bottom - top;
-	
 	if (width <= 0 || height <= 0) return;
-	
+
 	long depth = max(4L, min(width, height) / 4);
-	
+
 	long frontX1 = left;
 	long frontY1 = top + depth;
 	long frontX2 = right - depth;
 	long frontY2 = bottom;
-	
+
 	long backX1 = left + depth;
 	long backY1 = top;
 	long backX2 = right;
 	long backY2 = bottom - depth;
-	
-	HBRUSH hOldBrush = (HBRUSH)SelectObject(hdc, GetStockObject(NULL_BRUSH));
-	Rectangle(hdc, (int)backX1, (int)backY1, (int)backX2, (int)backY2);
-	Rectangle(hdc, (int)frontX1, (int)frontY1, (int)frontX2, (int)frontY2);
-	
+
+	long ox1 = xs1, oy1 = ys1, ox2 = xs2, oy2 = ys2;
+
+	Shape::Set(backX1, backY1, backX2, backY2);
+	RectShape::Show(hdc);
+
+	Shape::Set(frontX1, frontY1, frontX2, frontY2);
+	RectShape::Show(hdc);
+
+	Shape::Set(ox1, oy1, ox2, oy2);
+
 	MoveToEx(hdc, (int)frontX1, (int)frontY1, NULL); LineTo(hdc, (int)backX1, (int)backY1);
 	MoveToEx(hdc, (int)frontX2, (int)frontY1, NULL); LineTo(hdc, (int)backX2, (int)backY1);
 	MoveToEx(hdc, (int)frontX2, (int)frontY2, NULL); LineTo(hdc, (int)backX2, (int)backY2);
 	MoveToEx(hdc, (int)frontX1, (int)frontY2, NULL); LineTo(hdc, (int)backX1, (int)backY2);
-	
-	SelectObject(hdc, hOldBrush);
 }
 
 Shape* PointShape::Clone() const {
